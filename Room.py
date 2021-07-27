3356b729917a0b4c294a98e50dda8977a13d0f44
Jack <sirjackjones@outlookc.om>
2021-07-27 16:34:53 +0100

    pushing most recent changes and a little bit of tidying up the code

--- Room.py
+++ Room.py
@@ -49,9 +49,7 @@ class Room:
                 plug.display_smart_plugs()
 
     def replace_smart_plug_device(self, smart_plug_id_selection, device_replacement):
-        # i am almost certain the device_replacement wont work
         for plug in self._plug_list:
             if plug.get_smart_plug_id() == smart_plug_id_selection:
                 plug.set_smart_plug_device(device_replacement)
                 return
-
